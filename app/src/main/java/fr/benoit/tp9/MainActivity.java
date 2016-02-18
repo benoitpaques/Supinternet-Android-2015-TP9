1445e253027c702eab09a86553d32d30f548e7c5
benoit.paques <benoit.paques@gmail.com>
2016-02-18 11:20:48 +0100

    Correction TP9

---
+++ app/src/main/java/fr/benoit/tp9/MainActivity.java
@@ -0,0 +1,47 @@
+package fr.benoit.tp9;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SELECT_PICTURE = 1;
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void urlClick(View v){
+        String url = "http://www.supinternet.fr";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
+    public void imageClick(View v){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Select Picture"), SELECT_PICTURE);
+    }
+
+   public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                imageView.setImageURI(selectedImageUri);
+            }
+        }
+    }
+}
